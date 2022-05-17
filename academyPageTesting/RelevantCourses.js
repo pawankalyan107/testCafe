60ff6fb357502837df76dcea3a8acbd3c3e302b3
Pawankalyan Veldurthi <Pawan@blrgle.in>
2022-05-17 11:41:08 +0530

    updated course list functionality

--- academyPageTesting/RelevantCourses.js
+++ academyPageTesting/RelevantCourses.js
@@ -1,23 +1,22 @@
 import { Selector } from "testcafe";
 
 export async function RelevantCourses(t) {
-    const lengthOfList = await Selector(`#sliding-navigation > a:nth-child(n)`).count
-    for(let i=2; i<=lengthOfList-2; i++){
-        await t.click(Selector(`#sliding-navigation > a:nth-child(${i+1})`));
-        let data_rel = await Selector(`#courses-list-accordion > div:nth-child(${i}) > div`).getAttribute("data-rel")
+    const lengthOfCourseList = await Selector(`#sliding-navigation > a:nth-child(n)`).count
+    for(let index=2; index<=lengthOfCourseList-2; index++){
+        await t.click(Selector(`#sliding-navigation > a:nth-child(${index+1})`));
+        let dataAttribute = await Selector(`#courses-list-accordion > div:nth-child(${index}) > div`).getAttribute("data-rel")
         const cards = Selector(
-            `#courseSlider_gla_${data_rel} > div:nth-of-type(n) > .card`
+            `#courseSlider_gla_${dataAttribute} > div:nth-of-type(n) > .card`
         );
         let listCount = await cards.count;
         let j =2;
         while (listCount) {
-            let data_rell = await Selector(`#courses-list-accordion > div:nth-child(${i}) > div`).getAttribute("data-rel")
             const courseCards = await Selector(
-                `#courseSlider_gla_${data_rell} > div:nth-child(${j-1}) > div > a`
+                `#courseSlider_gla_${dataAttribute} > div:nth-child(${j-1}) > div > a`
             ).getAttribute('data-section');
             await t
                 .expect(
-                    Selector(`#sliding-navigation > a:nth-child(${i})`).getAttribute("data-domain-name")
+                    Selector(`#sliding-navigation > a:nth-child(${index})`).getAttribute("data-domain-name")
                 ).eql(courseCards);
             listCount--;
             j++
